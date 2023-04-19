8539e335e58795535adebfde85629d9205d33cc9
CorentinBLot3 <131261050+CorentinBLot3@users.noreply.github.com>
2023-04-19 13:55:00 +0200

    Add files via upload

---
+++ pages/Releves.php
@@ -0,0 +1,373 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<link rel="icon" type="image/png" sizes="16x16" href="../img/Logo_Arbre.png">
+	<link rel="stylesheet" href="../css/style.css">
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
+	<title>Relevés</title>
+</head>
+<body>
+	<header>
+		<img src="../img/Logo_Arbre.png" alt="">
+		<center><h1>GénéaLauret</h1></center>
+		<div class="menu">
+				<ul>
+					<li><a href="Accueil.php">Accueil</a></li>
+					<li><a href="Genea.php">Généalogie familiale</a></li>
+					<li><a href="Releves.php">Relevés généalogiques</a></li>
+					<li><a href="Contacts.php">Contacts</a></li>
+				</ul>
+			
+		</div>
+		
+		
+	</header>
+	
+	<div class="container">
+		<div class="contenu">
+		<center><h1>Relevés généalogiques</h1>
+		<br>
+    	<h4>Le site de Généalogie de la Famille Lauret</h4></center>
+		<br>
+		<p>
+		Ce tableau présente des informations sur les actes de naissance, mariage et décès <br>disponibles dans les registres de plusieurs communes en France. Les données sont <br> 
+		présentées sous forme de tableaux avec des informations sur les années couvertes et le <br> nombre d'actes disponibles pour chaque type d'événement. Les fichiers Excel sont<br> 
+		 également inclus pour chaque commune répertoriée. Les informations les plus récentes <br> datent de 2014 pour la commune de Sévérac le Château, tandis que les autres datent de<br>  2010. Ces données peuvent être utiles pour les personnes effectuant des recherches<br>  généalogiques dans ces régions.
+		<br><br><br>
+		</p>
+
+		<td>
+		<h4><a name="Lapanouse de Sévérac / Période 1792 - 1902">Relevés en Aveyron</a></h4></center>
+
+		
+		
+		<p>sur les communes Lapanouse,Lavernhe, Recoules Prévinquières,<br>
+		Sébazac Concourès, Sévérac le Château
+		</p>
+		<br>
+		<table class="search-table">
+		  <tbody>
+		    <tr>
+		      <td colspan="5" class="search-heading">
+		        Formulaire de recherche pour toutes ces communes : <br>
+		      </td>
+		    </tr>
+		    <tr>
+		      <td>
+		        <a href="RechercompilNaissances.asp">
+		          <span class="search-link">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- Naissances -</span>
+		        </a>
+		      </td>
+		    </tr>
+		    <tr>
+		      <td>
+		        <a href="RechercompilMariages.asp">
+		          <span class="search-link">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- Mariages -</span>
+		        </a>
+		      </td>
+		    </tr>
+		    <tr>
+		      <td>
+		        <a href="RecherCompilDeces.asp">
+		          <span class="search-link">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- Décès -</span>
+		        </a>
+		      </td>
+		    </tr>
+		  </tbody>
+		</table>
+		<br>
+		<table class="search-table">
+		  <tbody>
+		  	<tr>
+			  <td class="title" colspan="5">
+			    <span class="town-name">Lapanouse de Sévérac</span> 
+			    <span class="update">(mise à jour du 13/03/2010)</span>
+			    <span class="photos">
+			      <a href="Photos-Actes/Lapanouse/Photos-Registres.html">
+			        Photos de registres absents des CD
+			      </a>
+			    </span>
+			  </td>
+			</tr>
+			<tr>
+			    <td width="9%">&nbsp;</td>
+			    <td class="category-cell" width="8%">Naissances</td>
+			    <td class="data-cell" width="18%" align="center">1619 / 1912</td>
+			    <td class="data-cell" width="13%" align="right">8 099 actes</td>
+			    <td class="data-cell" width="13%" align="center">
+			        <font face="Times New Roman">
+			            <a href="depouillements/Lapanouse1619-1912N.zip">
+			                <font color="#0000FF">Fichier Excel</font>
+			            </a>
+			        </font>
+			    </td>
+			</tr>
+			<tr>
+			  <td width="9%">&nbsp;</td>
+			  <td class="border-cell" width="8%">Mariages</td>
+			  <td class="border-cell" width="18%" align="center">1619 / 1912</td>
+			  <td class="border-cell" width="13%" align="right">2 232 actes</td>
+			  <td class="border-cell" width="13%" align="center">
+			    <font face="Times New Roman">
+			      <a href="depouillements/Lapanouse1619-1912M.zip"><font color="#0000FF">Fichier Excel</font></a>
+			    </font>
+			  </td>
+			</tr>
+			<tr>
+			  <td width="9%">&nbsp;</td>
+			  <td class="border-cell" width="8%">Décès</td>
+			  <td class="border-cell" width="18%" align="center">1619 / 1912</td>
+			  <td class="border-cell" width="13%" align="right">6 521 actes</td>
+			  <td class="border-cell" width="13%" align="center">
+			    <font face="Times New Roman">
+			      <a href="depouillements/Lapanouse1619-1912D.zip"><font color="#0000FF">Fichier Excel</font></a>
+			    </font>
+			  </td>
+			</tr>
+		
+			<tr>
+				<td style="color: #800000" colspan="5"><font face="Arial">
+				<span style="font-size: 14pt">Lavernhe</span></font> <font class="font5">
+				(mise à jour du 13/03/2010)</font></td>
+			</tr>
+			<tr>
+				<td width="9%">&nbsp;</td>
+				<td style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px" width="8%" rowspan="2">
+				Naissances</td>
+				<td style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px" width="18%" align="center">
+				AN 4 / AN 11</td>
+				<td style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px" width="13%" align="right">
+				134 actes</td>
+				<td style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px" width="13%" align="center">
+				<font face="Times New Roman">
+				<a href="depouillements/LavernheAN04-AN11N.zip"><font color="#0000FF">Fichier Excel</font></a></font></td>
+			</tr>
+			<tr>
+				<td width="9%">&nbsp;</td>
+				<td style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px" width="18%" align="center">
+				1823 / 1832</td>
+				<td style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px" width="13%" align="right">
+				250 actes</td>
+				<td style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px" width="13%" align="center">
+				<font face="Times New Roman">
+				<a href="depouillements/Lavernhe1823-1832N.zip"><font color="#0000FF">Fichier Excel</font></a></font></td>
+			</tr>
+			<tr>
+				<td width="9%">&nbsp;</td>
+				<td style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px" width="8%" rowspan="2">
+				Mariages</td>
+				<td style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px" width="18%" align="center">
+				AN 03 / AN 11</td>
+				<td style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px" width="13%" align="right">
+				29 actes</td>
+				<td style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px" width="13%" align="center">
+				<font face="Times New Roman">
+				<a href="depouillements/LavernheAN03-AN11M.zip"><font color="#0000FF">Fichier Excel</font></a></font></td>
+			</tr>
+			<tr>
+				<td width="9%">&nbsp;</td>
+				<td style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px" width="18%" align="center">
+				1823 / 1832</td>
+				<td style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px" width="13%" align="right">
+				66 actes</td>
+				<td style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px" width="13%" align="center">
+				<font face="Times New Roman">
+				<a href="depouillements/Lavernhe1823-1832M.zip"><font color="#0000FF">Fichier Excel</font></a></font></td>
+			</tr>
+			<tr>
+				<td width="9%">&nbsp;</td>
+				<td style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px" width="8%" rowspan="2">
+				Décès</td>
+				<td style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px" width="18%" align="center">
+				AN 04 / AN 11</td>
+				<td style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px" width="13%" align="right">
+				72 actes</td>
+				<td style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px" width="13%" align="center">
+				<font face="Times New Roman">
+				<a href="depouillements/LavernheAN04-AN11D.zip"><font color="#0000FF">Fichier Excel</font></a></font></td>
+			</tr>
+			<tr>
+				<td width="9%">&nbsp;</td>
+				<td style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px" width="18%" align="center">
+				1823 / 1832</td>
+				<td style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px" width="13%" align="right">
+				187 actes</td>
+				<td style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px" width="13%" align="center">
+				<font face="Times New Roman">
+				<a href="depouillements/Lavernhe1823-1832D.zip"><font color="#0000FF">Fichier Excel</font></a></font></td>
+			</tr>
+			<tr>
+				<td width="9%">&nbsp;</td>
+				<td width="9%">&nbsp;</td>
+				<td width="19%">&nbsp;</td>
+				<td width="14%">&nbsp;</td>
+				<td width="14%" align="center">&nbsp;</td>
+			</tr>
+			<tr>
+				<td width="79%" colspan="5"><font face="Arial" color="#800000">
+				<span style="font-size: 14pt">Recoules Prévinquières</span></font> <font class="font5">
+				(mise à jour du 13/03/2010)</font></td>
+			</tr>
+			<tr>
+				<td width="9%" height="23">&nbsp;</td>
+				<td width="9%" height="23" style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px">
+				Naissances</td>
+				<td width="19%" height="23" style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px" align="center">
+				AN 08</td>
+				<td width="14%" height="23" style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px" align="right">
+				17 actes</td>
+				<td width="14%" height="23" style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px" align="center">
+				<font face="Times New Roman">
+				<a href="depouillements/RecoulesAN08N.zip"><font color="#0000FF">Fichier Excel</font></a></font></td>
+			</tr>
+			<tr>
+				<td width="9%">&nbsp;</td>
+				<td width="9%" style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px">
+				Mariages</td>
+				<td width="19%" style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px" align="center">
+				AN 07</td>
+				<td width="14%" style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px" align="right">
+				2 actes</td>
+				<td width="14%" style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px" align="center">
+				<font face="Times New Roman">
+				<a href="depouillements/RecoulesAN07M.zip"><font color="#0000FF">Fichier Excel</font></a></font></td>
+			</tr>
+			<tr>
+				<td width="9%">&nbsp;</td>
+				<td width="9%" style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px">
+				Décès</td>
+				<td width="19%" style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px" align="center">
+				AN 08</td>
+				<td width="14%" style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px" align="right">
+				10 actes</td>
+				<td width="14%" style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px" align="center">
+				<font face="Times New Roman">
+				<a href="depouillements/RecoulesAN08D.zip"><font color="#0000FF">Fichier Excel</font></a></font></td>
+			</tr>
+			<tr>
+				<td width="9%">&nbsp;</td>
+				<td width="9%">&nbsp;</td>
+				<td width="19%" align="center">&nbsp;</td>
+				<td width="14%" align="right">&nbsp;</td>
+				<td width="14%" align="center">&nbsp;</td>
+			</tr>
+			<tr>
+				<td width="94%" colspan="5">
+				<font face="Arial" style="font-size: 14pt" color="#800000">Sébazac 
+				Concourès</font> <font class="font5">(mise à jour du 13/03/2010)</font></td>
+			</tr>
+			<tr>
+				<td width="9%">&nbsp;</td>
+				<td width="9%" style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px">
+				Naissances</td>
+				<td width="19%" style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px" align="center">
+				1637 / 1902</td>
+				<td width="14%" style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px" align="right">
+				3 969 actes</td>
+				<td width="14%" style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px" align="center">
+				<font face="Times New Roman">
+				<a href="depouillements/Concoures1637-1902N.zip"><font color="#0000FF">Fichier Excel</font></a></font></td>
+			</tr>
+			<tr>
+				<td width="9%">&nbsp;</td>
+				<td width="9%" style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px">
+				Mariages</td>
+				<td width="19%" style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px" align="center">
+				1652 / 1902</td>
+				<td width="14%" style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px" align="right">
+				987 actes</td>
+				<td width="14%" style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px" align="center">
+				<font face="Times New Roman">
+				<a href="depouillements/Concoures1652-1902M.zip"><font color="#0000FF">Fichier Excel</font></a></font></td>
+			</tr>
+			<tr>
+				<td width="9%">&nbsp;</td>
+				<td width="9%" style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px">
+				Décès</td>
+				<td width="19%" style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px" align="center">
+				1638 / 1902</td>
+				<td width="14%" style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px" align="right">
+				2 870 actes</td>
+				<td width="14%" style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px" align="center">
+				<font face="Times New Roman">
+				<a href="depouillements/Concoures1638-1902D.zip"><font color="#0000FF">Fichier Excel</font></a></font></td>
+			</tr>
+			<tr>
+				<td width="9%">&nbsp;</td>
+				<td width="9%">&nbsp;</td>
+				<td width="19%">&nbsp;</td>
+				<td width="14%">&nbsp;</td>
+				<td width="14%" align="center">&nbsp;</td>
+			</tr>
+			<tr>
+				<td width="94%" colspan="5">
+				<font face="Arial" style="font-size: 14pt" color="#800000">Sévérac le 
+				Château</font> <font class="font5">(mise à jour du 19/10/2014)</font></td>
+			</tr>
+			<tr>
+				<td width="9%">&nbsp;</td>
+				<td width="9%" style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px">
+				Naissances</td>
+				<td width="19%" style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px">
+				<p align="center">AN 06 / AN 10</p></td>
+				<td width="14%" style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px" align="right">
+				255 actes</td>
+				<td width="14%" style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px" align="center">
+				<font face="Times New Roman">
+				<a href="depouillements/SeveracAN06a10N.zip"><font color="#0000FF">Fichier Excel</font></a></font></td>
+			</tr>
+			<tr>
+				<td width="9%">&nbsp;</td>
+				<td width="9%" style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px">
+				Mariages</td>
+				<td width="19%" style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px" align="center">
+				1610 / 1902</td>
+				<td width="14%" style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px" align="right">
+				10 572 actes</td>
+				<td width="14%" style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px" align="center">
+				<font face="Times New Roman">
+				<a href="depouillements/SeveracAN06a10M.zip"><font color="#0000FF">Fichier Excel</font></a> 
+				An06/10</font></td>
+			</tr>
+			<tr>
+				<td width="9%">&nbsp;</td>
+				<td width="9%" style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px">
+				Décès</td>
+				<td width="19%" style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px" align="center">
+				AN 06 / AN 10</td>
+				<td width="14%" style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px" align="right">
+				137 actes</td>
+				<td width="14%" style="border-style: solid; border-width: 1px; padding-left: 4px; padding-right: 4px; padding-top: 1px; padding-bottom: 1px" align="center">
+				<font face="Times New Roman">
+				<a href="depouillements/SeveracAN06a10D.zip"><font color="#0000FF">Fichier Excel</font></a></font></td>
+			</tr>
+		</tbody></table>
+		
+		<!--msnavigation--></td>
+		</div>
+	</div>
+	
+</body>
+<footer>
+		<div class="footer2">
+			<img src="../img/Logo_Arbre.png" alt="">
+			<div>
+				<h1>GénéaLauret</h1>
+				
+			</div>	
+			<br>
+			<div>
+				<h2>
+					<a href="pages/MentionsLégales.php">- Mentions légales -</a>
+				</h2>
+			</div>
+			<br>
+			<div>
+				<h2 style="padding-top: 45%;">
+					<a href="pages/Contact.php">- Contacts -</a>
+				</h2>
+			</div>
+		</div>
+	</footer>
+</html>
